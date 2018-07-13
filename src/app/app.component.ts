ec0ca229c563d9ef5a4c8b327fa9f9b6f13d1ea7
hajela123 <hajelarahul93@gmail.com>
2018-07-13 12:03:28 +0530

    bulb assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  bulbs=[0,0,0,0,0];
+  status1:boolean=true;
+  x=['./assets/bulb-off.png','./assets/bulb-off.png','./assets/bulb-off.png','./assets/bulb-off.png','./assets/bulb-off.png'];
+  y=['Off','Off','Off','Off','Off'];
+  ind;
+  switch(ind)
+{
+  if(this.status1==false){
+ this.status1=true;
+this.x[ind]='./assets/bulb-on.png';
+this.y[ind]='OFF';
+}
+else  {
+  this.status1=false;  
+  this.x[ind]='./assets/bulb-off.png';
+  this.y[ind]='On';
+} 
+}
+
+
 }
